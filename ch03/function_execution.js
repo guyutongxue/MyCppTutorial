4b2fcda5c40ac0d4528a924527c4c0528954e467
Guyutongxue <guyutongxue@163.com>
2020-07-19 23:23:15 +0800

    add a new animation

--- ch03/function_execution.js
+++ ch03/function_execution.js
@@ -1,169 +1,19 @@
-/**
- * 
- * @param {Paper} paper
- * @param {number} l 
- * @param {number} t 
- * @param {number} h 
- * @param {number} w 
- */
-function createRAM(paper, l, t, w, h) {
-    return paper.rect(l, t, w, h, 30).attr({
-        fill: '#daf2e8',
-        stroke: '#005',
-        'stroke-linejoin': 'round',
-        'stroke-width': 2,
-        title: '计算机的一片内存'
-    });
-}
-/**
- * 
- * @param {Paper} paper 
- * @param {string} name
- * @param {number} l  
- * @param {number} t 
- * @param {number} w 
- * @param {number} h 
- */
-function createFunction(paper, name, l, t, w, h) {
-    let func = paper.rect(l, t, w, h).attr({
-        fill: 'white',
-        stroke: '#7878de',
-        'stroke-width': 2,
-        title: `存放 ${name} 函数指令的空间`
-    });
-    let caption = paper.text(l + w / 2, t - 20, name).attr({
-        'font-size': 25,
-        'font-family': 'var(--code-font-family), monospace'
-    });
-    return paper.set().push(func, caption);
-}
-/**
- * Add codes to function figure.
- * @param {*} func 
- * @param {Array<string>} code 
- */
-function addCode(func, code) {
-    const dy = func.attr('height') / code.length;
-    const width = func.attr('width') - 10;
-    const l = func.attr('x') + 5;
-    const t = func.attr('y') + 5;
-    let text = func.paper.text(l, t, code.join('\n')).attr({
-        'text-anchor': 'start',
-        'font-family': 'var(--code-font-family), monospace'
-    });
-    Array.from(text.node.children).forEach((e, i) => {
-        e.setAttribute('dy', (i ? dy : dy / 2));
-        e.setAttribute('textLength', width);
-        e.setAttribute('lengthAdjust', 'spacingAndGlyphs');
-    });
-    return text;
-}
-class Arrow {
-    /**
-     * 
-     * @param {*} paper 
-     * @param {number} x1 
-     * @param {number} y1 
-     * @param {number} x2 
-     * @param {number} y2 
-     * @param {string} color 
-     */
-    constructor(paper, x1, y1, x2, y2, color) {
-        this.paper = paper;
-        this.x1 = x1;
-        this.x2 = x2;
-        this.y1 = y1;
-        this.y2 = y2;
-        if (arguments.length > 5)
-            this.color = color;
-        else
-            this.color = 'red';
-        this.element = paper.path(`M${x1},${y1}L${x2},${y2}`).attr({
-            stroke: this.color,
-            'stroke-width': 2,
-            'arrow-end': 'open-midium-long'
-        });
-    }
-    /**
-     * 
-     * @param {boolean} isAnime 
-     */
-    show(isAnime) {
-        if (isAnime === true) {
-            let startX = (this.x2 - this.x1) / 20 + this.x1;
-            let startY = (this.y2 - this.y1) / 20 + this.y1;
-            this.element.attr('path', `M${this.x1},${this.y1}L${startX},${startY}`);
-            this.element.show();
-            this.element.animate({ 'path': `M${this.x1},${this.y1}L${this.x2},${this.y2}` }, 500, 'linear');
-        } else {
-            this.element.show();
-        }
-    }
-    hide() {
-        this.element.hide();
-    }
-}
-class Button {
-    /**
-     * 
-     * @param {*} paper 
-     * @param {HTMLElement} textElement
-     * @param {number} l 
-     * @param {number} t 
-     * @param {Array<string>} infoArr 
-     * @param {Array<()=>void>} eventArr what should happen before switching to this index
-     */
-    constructor(paper, textElement, l, t, infoArr, eventArr) {
-        this.paper = paper;
-        this.textElement = textElement;
-        this.l = l;
-        this.t = t;
-        this.num = infoArr.length;
-        this.infoArr = infoArr;
-        this.eventArr = eventArr;
-        this.step = this.num - 1;
-        this.set;
-    }
-    insert() {
-        let buttonElement = this.paper.rect(this.l, this.t, 100, 50).attr({
-            fill: 'lightgrey',
-            stroke: 'black'
-        });
-        let buttonText = this.paper.text(this.l + 50, this.t + 25, '开始').attr('font-size', 25);
-        let next = () => {
-            this.step++;
-            if (this.step === this.num) {
-                this.step = 0;
-                buttonText.attr('text', '开始');
-            }
-            if (this.step === 1) {
-                buttonText.attr('text', '下一步');
-            }
-            if (this.step + 1 === this.num) {
-                buttonText.attr('text', '复原');
-            }
-            this.textElement.textContent = this.infoArr[this.step];
-            this.eventArr[this.step]();
-        };
-        this.set = this.paper.set().push(buttonElement, buttonText).click(next);
-        next();
-    }
-}
+import { Arrow, Button, StatementSnippet, VariableSnippet, addCode, createFunction, createRAM } from './raphael_util.js';
 
 // Fig. 1
 {
     document.querySelector('#fig1').setAttribute('style', 'min-width: 550px');
-    let fig1 = Raphael(document.querySelector('#fig1'), 550, 400);
-    let ram = createRAM(fig1, 20, 50, 300, 300);
-    let main = createFunction(fig1, 'main', 40, 100, 100, 200);
+    let fig = Raphael(document.querySelector('#fig1'), 550, 400);
+    let ram = createRAM(fig, 20, 50, 300, 300);
+    let main = createFunction(fig, 'main', 40, 100, 100, 200);
     const mainCode = [
         'int a, b;         ',
         'cin >> a >> b;    ',
         'int c{a + b};     ',
         'cout << c << endl;'
     ];
-    let mainText = addCode(main[0], mainCode);
-    let mainArrow = new Arrow(fig1, 90, 105, 90, 295);
+    addCode(main, mainCode);
+    let mainArrow = new Arrow(fig, 90, 105, 90, 295);
     const info = [
         '',
         '计算机首先找到一片完整的内存。',
@@ -174,37 +24,35 @@ class Button {
         () => {
             ram.hide();
             main.hide();
-            mainText.hide();
             mainArrow.hide();
         },
-        () => { 
+        () => {
             ram.show();
         },
         () => {
             main.show();
-            mainText.show();
         },
         () => {
             mainArrow.show(true);
         }
     ];
-    new Button(fig1, document.querySelector('#fig1Text'), 400, 300, info, eventArr).insert();
+    new Button(fig, document.querySelector('#fig1Text'), 400, 300, info, eventArr).insert();
 }
 
 // Fig. 2
 {
     document.querySelector('#fig2').setAttribute('style', 'min-width: 550px');
-    let fig1 = Raphael(document.querySelector('#fig2'), 550, 400);
-    let ram = createRAM(fig1, 20, 50, 300, 300);
-    let main = createFunction(fig1, 'main', 40, 100, 100, 200);
-    let max = createFunction(fig1, 'max', 160, 100, 100, 200);
+    let fig = Raphael(document.querySelector('#fig2'), 550, 400);
+    let ram = createRAM(fig, 20, 50, 300, 300);
+    let main = createFunction(fig, 'main', 40, 100, 100, 200);
+    let max = createFunction(fig, 'max', 160, 100, 100, 200);
     const mainCode = [
         'int a, b, c;      ',
         'cin >> a >> b;    ',
         'c = max(a, b);    ',
         'cout << c << endl;'
     ];
-    let mainText = addCode(main[0], mainCode);
+    addCode(main, mainCode);
     const maxCode = [
         'int z;             ',
         'if (x > y)         ',
@@ -213,12 +61,12 @@ class Button {
         '    z = y;         ',
         'return z;          '
     ]
-    let maxText = addCode(max[0], maxCode);
-    let topArrow = new Arrow(fig1, 90, 105, 90, 225);
-    let inArrow = new Arrow(fig1, 90, 225, 210, 105);
-    let maxArrow = new Arrow(fig1, 210, 105, 210, 295);
-    let outArrow = new Arrow(fig1, 210, 295, 90, 235);
-    let bottomArrow = new Arrow(fig1, 90, 235, 90, 295);
+    addCode(max, maxCode);
+    let topArrow = new Arrow(fig, 90, 105, 90, 225);
+    let inArrow = new Arrow(fig, 90, 225, 210, 105);
+    let maxArrow = new Arrow(fig, 210, 105, 210, 295);
+    let outArrow = new Arrow(fig, 210, 295, 90, 235);
+    let bottomArrow = new Arrow(fig, 90, 235, 90, 295);
     const info = [
         '',
         '计算机首先找到一片完整的内存。',
@@ -236,27 +84,23 @@ class Button {
             ram.hide();
             main.hide();
             max.hide();
-            mainText.hide();
-            maxText.hide();
             inArrow.hide();
             maxArrow.hide();
             outArrow.hide();
             topArrow.hide();
             bottomArrow.hide();
         },
-        () => { 
-            ram.show(); 
+        () => {
+            ram.show();
         },
         () => {
             main.show();
-            mainText.show();
         },
         () => {
             topArrow.show(true);
         },
         () => {
             max.show();
-            maxText.show();
         },
         () => {
             inArrow.show(true);
@@ -269,7 +113,6 @@ class Button {
         },
         () => {
             max.hide();
-            maxText.hide();
             inArrow.hide();
             maxArrow.hide();
             outArrow.hide();
@@ -278,5 +121,98 @@ class Button {
             bottomArrow.show(true);
         }
     ];
-    new Button(fig1, document.querySelector('#fig2Text'), 400, 300, info, eventArr).insert();
+    new Button(fig, document.querySelector('#fig2Text'), 400, 300, info, eventArr).insert();
+}
+
+// Fig. 3
+{
+    document.querySelector('#fig3').setAttribute('style', 'min-width: 550px');
+    let fig = Raphael(document.querySelector('#fig3'), 550, 400);
+    let ram = createRAM(fig, 20, 50, 300, 300);
+    let main = createFunction(fig, 'main', 40, 100, 120, 200);
+    let change = createFunction(fig, 'change', 180, 100, 120, 200);
+    let call = new StatementSnippet(main, 10, 120, 100, 30, 'change(a,b);');
+    let cout = new StatementSnippet(main, 10, 160, 100, 30, 'cout<<a<<b;');
+    let assign = new StatementSnippet(change, 10, 120, 100, 30, 'c=30,d=50;')
+    let arrows = [
+        new Arrow(fig, 100, 105, 100, 225),
+        new Arrow(fig, 100, 225, 240, 105),
+        new Arrow(fig, 240, 105, 240, 295),
+        new Arrow(fig, 240, 295, 100, 250),
+        new Arrow(fig, 100, 250, 100, 295)
+    ];
+    let a = new VariableSnippet(main, 10, 10, 100, 30, 'a');
+    let b = new VariableSnippet(main, 10, 50, 100, 30, 'b');
+    let c = new VariableSnippet(change, 10, 10, 100, 30, 'c');
+    let d = new VariableSnippet(change, 10, 50, 100, 30, 'd');
+    const info = [
+        '',
+        '计算机首先找到一片完整的内存。',
+        '计算机在内存中为 main 函数开辟一片空间。',
+        '存储变量 a 和 b。一般地，在函数体内定义的变量将存储在函数独有的空间中，所以图中画在了 main 函数里面。',
+        '开始调用 change 函数，为它开辟一片空间……',
+        '按照箭头所述方向执行。首先初始化形参 c 和 d。',
+        'change 函数为变量 c 和 d 赋值。',
+        'change 函数执行完成后，其所在的存储空间被立即释放。',
+        '完成 main 函数余下过程，输出 a 和 b 的值。'
+    ]
+    const eventArr = [
+        () => {
+            ram.hide();
+            main.hide();
+            change.hide();
+            a.hide(), b.hide(), c.hide(), d.hide();
+            call.hide(), cout.hide(), assign.hide();
+            arrows.forEach((e) => { e.hide(); });
+        },
+        () => {
+            // 
+            ram.show();
+        },
+        () => {
+            main.show();
+        },
+        () => {
+            a.show();
+            b.show();
+            a.setValue('3');
+            b.setValue('5');
+        },
+        () => {
+            call.show();
+            change.show();
+        },
+        async () => {
+            c.show();
+            d.show();
+            c.setValue('3');
+            d.setValue('5');
+
+            // make arrow blink (really ugly implement)
+            let sh = setInterval(() => {
+                arrows.forEach(e => { e.show(); });
+            }, 400);
+            await new Promise(r => setTimeout(r, 200));
+            let hi = setInterval(() => {
+                arrows.forEach(e => { e.hide(); });
+            }, 400)
+            await new Promise(r => setTimeout(r, 1200));
+            clearInterval(sh), clearInterval(hi);
+            arrows.forEach(e => { e.hide(); });
+        },
+        () => {
+            assign.show();
+            c.setValue('30');
+            d.setValue('50');
+        },
+        () => {
+            c.hide(), d.hide();
+            assign.hide();
+            change.hide();
+        },
+        () => {
+            cout.show();
+        },
+    ];
+    new Button(fig, document.querySelector('#fig3Text'), 400, 300, info, eventArr).insert();
 }
---
+++ ch03/raphael_util.js
@@ -0,0 +1,260 @@
+/**
+ * 
+ * @param {import("raphael").RaphaelPaper} paper
+ * @param {number} l 
+ * @param {number} t 
+ * @param {number} w 
+ * @param {number} h 
+ * @returns {import("raphael").RaphaelElement} 
+ */
+function createRAM(paper, l, t, w, h) {
+    return paper.rect(l, t, w, h, 30).attr({
+        fill: '#daf2e8',
+        stroke: '#005',
+        'stroke-linejoin': 'round',
+        'stroke-width': 2,
+        title: '计算机的一片内存'
+    });
+}
+
+class Snippet {
+    /**
+     * 
+     * @param {import("raphael").RaphaelElement} ele Graph of snippet
+     * @param {string} text Text of snippet
+     */
+    constructor(ele, mx, my, text) {
+        this.element = ele;
+        this.info = ele.paper.text(mx, my, text).attr({
+            'font-size': 18,
+            'font-family': 'var(--code-font-family), monospace'
+        });
+        this.content = ele.paper.set().push(ele, this.info);
+    }
+    /**
+     * 
+     * @param {string} text 
+     * @param {boolean} isStrong
+     * @return {void}
+     */
+    setText(text, isStrong) {
+        this.info.attr('text', text);
+        if (isStrong === true) {
+            this.info.stop();
+            this.info.attr({
+                'fill': 'red'
+            });
+            this.info.animate(Raphael.animation({
+                'fill': 'black'
+            }, 500, 'ease-in').delay(500));
+        }
+    }
+    show() {
+        this.content.show();
+    }
+    hide() {
+        this.content.hide();
+    }
+}
+
+class VariableSnippet extends Snippet {
+    /**
+     * 
+     * @param {import("raphael").RaphaelSet|import("raphael").RaphaelElement} func 
+     * @param {number} dx 
+     * @param {number} dy 
+     * @param {number} w 
+     * @param {number} h 
+     * @param {string} name 
+     */
+    constructor(func, dx, dy, w, h, name) {
+        if (func.type === 'set') {
+            func = func[0];
+        }
+        let x = func.attr('x');
+        let y = func.attr('y');
+        let ele = func.paper.rect(x + dx, y + dy, w, h).attr({
+            fill: 'white',
+            stroke: '#7878de',
+        });
+        super(ele, x + dx + w / 2, y + dy + h / 2, name);
+        this.name = name;
+    }
+    /**
+     * 
+     * @param {string} value 
+     */
+    setValue(value) {
+        this.setText(`${this.name} : ${value}`, true);
+    }
+}
+
+class StatementSnippet extends Snippet {
+    /**
+     * 
+     * @param {import("raphael").RaphaelSet} func 
+     * @param {number} dx 
+     * @param {number} dy 
+     * @param {number} w 
+     * @param {number} h 
+     * @param {string} name 
+     */
+    constructor(func, dx, dy, w, h, name) {
+        let x = func[0].attr('x');
+        let y = func[0].attr('y');
+        let delta = 10;
+        let ele = func[0].paper.path(`M${x + dx + delta} ${y + dy} h ${w - delta} l ${-delta} ${h} h ${-w + delta} Z`).attr({
+            fill: 'white',
+            stroke: '#7878de',
+        });
+        super(ele, x + dx + w / 2, y + dy + h / 2, name);
+        this.name = name;
+        this.setText(name, false);
+        Array.from(this.info.node.children).forEach((e) => {
+            e.setAttribute('textLength', `${w - delta}`);
+            e.setAttribute('lengthAdjust', 'spacingAndGlyphs');
+        });
+    }
+}
+
+/**
+ * 
+ * @param {import("raphael").RaphaelPaper} paper 
+ * @param {string} name
+ * @param {number} l  
+ * @param {number} t 
+ * @param {number} w 
+ * @param {number} h 
+ * @returns {import("raphael").RaphaelSet}
+ */
+function createFunction(paper, name, l, t, w, h) {
+    let func = paper.rect(l, t, w, h).attr({
+        fill: 'white',
+        stroke: '#7878de',
+        'stroke-width': 2,
+        title: `存放 ${name} 函数指令的空间`
+    });
+    let caption = paper.text(l + w / 2, t - 20, name).attr({
+        'font-size': 25,
+        'font-family': 'var(--code-font-family), monospace'
+    });
+    return paper.set().push(func, caption);
+}
+/**
+ * Add codes to function figure.
+ * @param {import("raphael").RaphaelSet} func 
+ * @param {Array<string>} code 
+ * @returns {void}
+ */
+function addCode(func, code) {
+    const dy = func[0].attr('height') / code.length;
+    const width = func[0].attr('width') - 10;
+    const l = func[0].attr('x') + 5;
+    const t = func[0].attr('y') + 5;
+    let text = func.paper.text(l, t, code.join('\n')).attr({
+        'text-anchor': 'start',
+        'font-family': 'var(--code-font-family), monospace'
+    });
+    text.node.setAttribute('xml:space', 'preserve');
+    Array.from(text.node.children).forEach((e, i) => {
+        e.setAttribute('dy', (i ? dy : dy / 2));
+        e.setAttribute('textLength', width);
+        e.setAttribute('lengthAdjust', 'spacingAndGlyphs');
+
+    });
+    func.push(text);
+}
+class Arrow {
+    /**
+     * 
+     * @param {import("raphael").RaphaelPaper} paper 
+     * @param {number} x1 
+     * @param {number} y1 
+     * @param {number} x2 
+     * @param {number} y2 
+     * @param {string} [color='red'] 
+     */
+    constructor(paper, x1, y1, x2, y2, color) {
+        this.paper = paper;
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+        if (arguments.length > 5)
+            this.color = color;
+        else
+            this.color = 'red';
+        this.element = paper.path(`M ${x1} ${y1} L ${x2} ${y2}`).attr({
+            stroke: this.color,
+            'stroke-width': 2,
+            'arrow-end': 'open-midium-long'
+        });
+    }
+    /**
+     * 
+     * @param {boolean} [isAnime=false] 
+     */
+    show(isAnime) {
+        if (isAnime === true) {
+            this.element.stop();
+            let startX = (this.x2 - this.x1) / 20 + this.x1;
+            let startY = (this.y2 - this.y1) / 20 + this.y1;
+            this.element.attr('path', `M ${this.x1} ${this.y1} L ${startX} ${startY}`);
+            this.element.show();
+            this.element.animate({ 'path': `M ${this.x1} ${this.y1} L ${this.x2} ${this.y2}` }, 500, 'linear');
+        } else {
+            this.element.show();
+        }
+    }
+    hide() {
+        this.element.hide();
+    }
+}
+class Button {
+    /**
+     * 
+     * @param {import("raphael").RaphaelPaper} paper 
+     * @param {HTMLElement} textElement
+     * @param {number} l 
+     * @param {number} t 
+     * @param {Array<string>} infoArr 
+     * @param {Array<()=>void>} eventArr what should happen before switching to this index
+     */
+    constructor(paper, textElement, l, t, infoArr, eventArr) {
+        this.paper = paper;
+        this.textElement = textElement;
+        this.l = l;
+        this.t = t;
+        this.num = infoArr.length;
+        this.infoArr = infoArr;
+        this.eventArr = eventArr;
+        this.step = this.num - 1;
+        this.set;
+    }
+    insert() {
+        let buttonElement = this.paper.rect(this.l, this.t, 100, 50).attr({
+            fill: 'lightgrey',
+            stroke: 'black'
+        });
+        let buttonText = this.paper.text(this.l + 50, this.t + 25, '开始').attr('font-size', 25);
+        let next = () => {
+            this.step++;
+            if (this.step === this.num) {
+                this.step = 0;
+                buttonText.attr('text', '开始');
+            }
+            if (this.step === 1) {
+                buttonText.attr('text', '下一步');
+            }
+            if (this.step + 1 === this.num) {
+                buttonText.attr('text', '复原');
+            }
+            this.textElement.textContent = this.infoArr[this.step];
+            this.eventArr[this.step]();
+        };
+        this.set = this.paper.set().push(buttonElement, buttonText).click(next);
+        next();
+    }
+}
+
+export { Arrow, Button, StatementSnippet, VariableSnippet, addCode, createFunction, createRAM };
